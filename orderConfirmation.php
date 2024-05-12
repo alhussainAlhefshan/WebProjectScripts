81db7b9f70e90666de730400270e591a94b748c7
AH <138318188+alhussainAlhefshan@users.noreply.github.com>
2024-05-12 14:36:14 -0400

    orderConfirmation.php

--- orderConfirmation.php
+++ orderConfirmation.php
@@ -5,18 +5,6 @@
     <title>Order Confirmation - TUWAIQ</title>
     <meta http-equiv="refresh" content="4;url=index.php"> <!-- Redirect to index.php after 4 seconds -->
 
-    <!-- Meta and styles matching index.php -->
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
@@ -33,16 +21,6 @@
     </div>
 </footer>
 
-<!-- Vendor JS Files -->
-<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-<!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
 
 </body>
 </html>
